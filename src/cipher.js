4a871226d87acb8799824e0c5601e6ab2e474ecb
carolinaolv <carolinaolatevallejos96@gmail.com>
2018-12-03 12:57:33 -0300

    mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,35 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+encode: (displace,capLetter) => {
+  /* Acá va tu código */
+  let result = '';
+  for (let i=0; i<capLetter.length; i++){
+    let asciiNumber = capLetter.charCodeAt(i);
+    if (asciiNumber >= 65 && asciiNumber <= 90 ) {
+      let convertedLetter=(asciiNumber-65+parseInt(displace))%26+65;
+      result += String.fromCharCode(convertedLetter);
+    }
+    else {
+      result += String.fromCharCode(asciiNumber);
+    }
   }
-};
+  return result;
+},
+
+decode: (displace,capLetter) => {
+  let result = '';
+  for (let i=0; i<capLetter.length; i++){
+    let asciiNumber = capLetter.charCodeAt(i);
+    if (asciiNumber >= 65 && asciiNumber <= 90 ) {
+      let convertedLetter=(asciiNumber-65-parseInt (displace))%26+65;
+      result += String.fromCharCode(convertedLetter);
+    }
+    else {
+      result += String.fromCharCode(asciiNumber);
+    }
+  }
+  return result;
+
+}
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 /* Acá va tu código */
+function cifrar(){
+    let capLetter= document.getElementById("mensaje").value;
+    let displace= document.getElementById("numero").value;
+
+    let result= window.cipher.encode(displace,capLetter);
+    
+    document.getElementById("resultado").innerHTML = result;
+
+};
+
+function descifrar(){
+    let capLetter= document.getElementById("mensaje").value;
+    let displace= document.getElementById("numero").value;
+    
+    let result= window.cipher.decode(displace,capLetter);
+
+    document.getElementById("resultado").innerHTML = result;
+
+};
+
