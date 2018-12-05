3180e116d9328187debef17f0bf40bd5e2d2294b
carolinaolv <carolinaolatevallejos96@gmail.com>
2018-12-05 20:21:31 -0300

    arreglos de css

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 /* Acá va tu código */
-function cifrar(){
+//function cifrar(){
+    document.getElementById("btnIngresar").addEventListener("click",() =>{
+        //evento.preventDefault();
     let capLetter= document.getElementById("mensaje").value;
     let displace= document.getElementById("numero").value;
 
     let result= window.cipher.encode(displace,capLetter);
     
     document.getElementById("resultado").innerHTML = result;
+    })
 
-};
 
-function descifrar(){
+
+
+//function descifrar(){
+    document.getElementById("btnIngresar2").addEventListener("click",() =>{
+        //evento.preventDefault();
     let capLetter= document.getElementById("mensaje").value;
     let displace= document.getElementById("numero").value;
     
     let result= window.cipher.decode(displace,capLetter);
 
     document.getElementById("resultado").innerHTML = result;
-
-};
+    })
 
