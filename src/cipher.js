104fc1ed931c4e224017bca46a7856d025c3ea9f
carolinaolv <carolinaolatevallejos96@gmail.com>
2018-12-11 08:28:00 -0300

    agregando minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -2,15 +2,21 @@ window.cipher = {
 encode: (displacement,capitalLetter) => {
   /* Acá va tu código */
   let result = '';
-  for (let i=0; i<capitalLetter.length; i++){
-    let asciiNumber = capitalLetter.charCodeAt(i);
-    if (asciiNumber >= 65 && asciiNumber <= 90 ) {
-      let convertedLetter=(asciiNumber-65+parseInt(displacement))%26+65;
+  for (let i=0; i<capitalLetter.length; i++){ //para recorrer el string, i representa cada letra de la palabra y length para que se detenga y ++ para que vaya sumando 1 
+    let asciiNumber = capitalLetter.charCodeAt(i); //charcodeat devuelve la posicion de cada letra
+    if (asciiNumber >= 65 && asciiNumber <= 90 ) { //para saber si la letra mayúscula se encuentra en este rango
+      let convertedLetter=(asciiNumber-65+parseInt(displacement))%26+65; //parse es para obtener el valor numerico del string
 
-      result += String.fromCharCode(convertedLetter);
+      result += String.fromCharCode(convertedLetter); //+=es para traerme la cadena completa
+    
     }
-    else {
-      result += String.fromCharCode(asciiNumber);
+        else if (asciiNumber >= 97 && asciiNumber <= 122 ) {
+          let convertedLetter=(asciiNumber-97+parseInt(displacement))%26+97;
+
+          result += String.fromCharCode(convertedLetter);
+        }else{
+          result += String.fromCharCode(asciiNumber);
+  
     }
   }
   return result;
